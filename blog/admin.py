c7bbb31336d855e86194cf184d855c4b92fa1c05
CaraMcAvinchey <cara.mcavinchey@gmail.com>
2022-12-04 15:49:40 +0000

    Add admin panel with additonal blog functions

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    summernote_fields = ('content')
