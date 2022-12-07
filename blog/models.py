c79e3cc131268c70f1b39493f16132132a4fcb3b
CaraMcAvinchey <cara.mcavinchey@gmail.com>
2022-12-07 17:09:23 +0000

    Fix error with custom model

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
@@ -10,13 +11,47 @@ class Plant(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts")
-    updated_on = models.DateTimeField(auto_now=True)
-    content = models.TextField()
-    featured_image = CloudinaryField('image', default='placeholder')
-    excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
+    updated_on = models.DateTimeField(auto_now=True)
+    featured_image = CloudinaryField('image', default='placeholder')
+    content = models.TextField()
+    excerpt = models.TextField(blank=True)
     likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    PLANT_LEVEL_CHOICES = [
+        ("1", "Super hearty (it will survive anyone!)"),
+        ("2", "Needs minor super vision, like a young adult"),
+        ("3", "Must be checked up on at least once a week"),
+        ("4", "Fussy, needs constant attention"),
+    ]
+
+    plant_level = models.CharField(
+        max_length=250,
+        choices=PLANT_LEVEL_CHOICES,
+        default="1",
+        null=False
+    )
+
+    PLANT_LIGHT_CHOICES = [
+        ("BRIGHT", "Bright, indirect light required"),
+        ("DIRECT", "Direct light required"),
+        ("LOW", "Low light required"),
+        ("MEDIUM", "Medium light required"),
+    ]
+
+    plant_light = models.CharField(
+        max_length=250,
+        choices=PLANT_LIGHT_CHOICES,
+        default="1",
+        null=False
+    )
+
+    plant_thirst = models.DecimalField(
+        null=True,
+        decimal_places=1,
+        max_digits=3,
+    )
 
     class Meta:
         ordering = ['-created_on']
