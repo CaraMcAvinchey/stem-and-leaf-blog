5e1bef5ab61c85f5c72771b5486abec64bb5daac
CaraMcAvinchey <cara.mcavinchey@gmail.com>
2022-12-09 08:44:46 +0000

    Add plant post detail page template

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.PlantList.as_view(), name='home')
+    path('', views.PlantList.as_view(), name='home'),
+    path('<slug:slug>/', views.PlantDetail.as_view(), name='post_detail'),
 ]
