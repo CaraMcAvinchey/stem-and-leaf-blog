8454d8afedf42f0e6677db49b5c6f20d2f370a2a
CaraMcAvinchey <cara.mcavinchey@gmail.com>
2022-12-16 15:53:16 +0000

    Fix error on form submission

--- blog/views.py
+++ blog/views.py
@@ -31,3 +31,34 @@ class PlantDetail(View):
                 "comment_form": CommentForm()
             },
         )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Plant.objects.filter(status=1)
+        plant = get_object_or_404(queryset, slug=slug)
+        comments = plant.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if plant.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = plant
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "plant": plant,
+                "comments": comments,
+                "commented": True,
+                "comment_form": comment_form,
+                "liked": liked
+            },
+        )
