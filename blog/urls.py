210ee75616288e515b2d26a3e6c773f96a5e8f33
CaraMcAvinchey <cara.mcavinchey@gmail.com>
2022-12-18 18:05:08 +0000

    Add blog posts to page

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.PlantList.as_view(), name='home'),
     path('<slug:slug>/', views.PlantDetail.as_view(), name='post_detail'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
